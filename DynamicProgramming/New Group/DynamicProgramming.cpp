3aca7f81ba8ba975e480345b73d76cdc36c9beb0
firmiana-caesar <firmiana-caesar@outlook.com>
2018-04-07 15:23:16 +0800

    dp update

--- DynamicProgramming/New Group/DynamicProgramming.cpp
+++ DynamicProgramming/New Group/DynamicProgramming.cpp
@@ -101,7 +101,7 @@ using namespace std;
 #define MAX_W 10000
 int n, W;
 int w[MAX_N], v[MAX_N];
-int dp[MAX_N][MAX_W];
+int dp[MAX_N + 1][MAX_W + 1];
 
 int rec(int i , int j){
     
@@ -145,3 +145,18 @@ int main(){
     return 0;
 }
 
+/*
+ int dp[MAX_N + 1][MAX_W + 1];
+ void solve(){
+ for (int i = n - 1; i >= 0 ; i--){
+    for (int j = 0; j <= W; j++){
+        if(j < w[i]){
+            dp[i][j] = dp[i + 1][j];
+        }else{
+            dp[i][j] = max(dp[i + 1][j], dp[i + 1][j - w[i]] + v[i]);
+        }
+    }
+ }
+ cout << dp[0][W];
+ }
+*/
